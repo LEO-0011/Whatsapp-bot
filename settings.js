aded17dfe9a82049e16c66bf27f0cf934d7d53ab
LEO-0011 <leoyagami123123@gmail.com>
2025-11-25 13:10:01 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,14 +3,14 @@ const settings = {
   author: '‎',
   botName: "⚘ᴩᴀᴛʜɪʀᴀᴊ.Py™♨ヅ-ꜱᴇʀᴠᴇʀ⚡",
   botOwner: '⚘ᴩᴀᴛʜɪʀᴀᴊ.Py™♨ヅ', // Your name
-  ownerNumber: '919751755841', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '91xxxxxxxxx', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20,
   storeWriteInterval: 10000,
   description: "This is a bot for managing group commands and automating tasks.",
   version: "3.0.4",
-  updateZipUrl: "https://github.com/mruniquehacker/Knightbot-MD/archive/refs/heads/main.zip",
+  updateZipUrl: "https://github.com/LEO-0011/Whatsapp-bot",
 };
 
 module.exports = settings;
